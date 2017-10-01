12d31bc47efac5b5b908e2d59b799b8e2cd0f473
Nathan N <Nitharios@users.noreply.github.com>
2017-09-30 19:56:54 -1000

    Flood command works!

--- flood-client.js
+++ flood-client.js
@@ -2,16 +2,24 @@
 const net = require('net');
 const PORT = process.env.PORT || 6969;
 
-const client = new net.connect(PORT, () => {
+const client = new net.connect(PORT, '10.0.1.161', () => {
   console.log(`Connected to server at port ${PORT}`);
 
-  // |---- readable
-  // v                 v---- writable
-  process.stdin.pipe(client);
+  process.stdin.on('readable', () => {
+    const data = process.stdin.read();
+    if (data !== null && data.toString().trim() === '\\flood') {
+      while (true) client.write('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?');
+      // client.pipe(client);
+    }
+  });
 
-  // |---- readable
-  // v         v---- writable
-  client.pipe(process.stdout);
+  // // |---- readable
+  // // v                 v---- writable
+  // process.stdin.pipe(client);
+
+  // // |---- readable
+  // // v         v---- writable
+  // client.pipe(process.stdout);
 });
 
 client.on('error', (err) => {
